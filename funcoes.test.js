b32e2f9674ef26efc7b0d44e23653c79273eca74
ale <128554136+AlePereiras@users.noreply.github.com>
2023-09-22 16:26:06 -0300

    teste

--- funcoes.test.js
+++ funcoes.test.js
@@ -1,4 +1,4 @@
-const { calcularINSS } = require("./script");
+const { calcularINSS } = require("./script.js");
 var assert = require('assert');
 
 assert(calcularINSS(1600,124.20)=="certo","O valor não está batendo");
